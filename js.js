992a8b99cc4c811d5f4c97941ad038bba3ec753b
AnastasiyaPozyomina <pozyomina@gmail.com>
2019-11-27 16:02:17 +0700

    sprites

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 // таймер обратного отчета
 // В теле функции initializeTimer мы инициализируем наш таймер и пишем логику изменений чисел в случае когда одно из них становится равным нулём:
 function initializeTimer() {
-	var endDate = new Date(2019,11,31,00,00);
+	var endDate = new Date(2020,0,1,00,00);
  
 	var currentDate = new Date();
 	var seconds = (endDate-currentDate) / 1000;
