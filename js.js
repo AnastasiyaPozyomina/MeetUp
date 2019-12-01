9f67f4449f8718261db99bdf50817de5e212782d
AnastasiyaPozyomina <pozyomina@gmail.com>
2019-12-02 01:24:44 +0700

    error correction

--- js.js
+++ js.js
@@ -3,27 +3,17 @@
 
   let mapElem = document.getElementById('map');
   let btnElem = mapElem.querySelector('.btn-map');
-  let paneElem = document.getElementById('pane');
+  let paneElem = document.getElementById('view-map');
   let rembtnElem = paneElem.querySelector('.remove-button');
 
 // открытие карты через обработчик события и display:block
 
 btnElem.onclick = function() {
   mapElem.classList.toggle('open');
-
 };
 
   
-
-
-// удалениие карты и автоматическая перезагрузка страницы
 rembtnElem.onclick = function() {
  mapElem.classList.toggle('open');
 };
 
-
- 
-  
-     
-	
-	
